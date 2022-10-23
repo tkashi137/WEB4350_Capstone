5032b2153428c7dc07ddf8ba71270ab446fe8a15
Alaina Larsen <alainalarsen@mail.weber.edu>
2022-10-22 23:54:30 -0600

    Revert "first commit"
    This reverts commit 748eed2202d6bfe2372c95951a4a6aa27dcad09a.

--- budget/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Label, Transaction
-
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Label)
-admin.site.register(Transaction)
