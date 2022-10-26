deb0796d9b45b33b49758bd4b9ecffb5cabd0675
Alaina Larsen <alainalarsen@mail.weber.edu>
2022-10-25 23:07:40 -0600

    added users app

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'budget.apps.BudgetConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ users/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+from django.template import loader
+from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
+from django.apps import apps
+
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {name}! You are logged in.')
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profilepage(request):
+    return render(request, 'users/profile.html')
