140aefcda1fec4c2a0ede8a92074c7cb0841f5d8
jamictaylor <jamitaylor@mail.weber.edu>
2022-11-08 16:40:32 -0700

    jami

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#login redirect to budget
+LOGIN_REDIRECT_URL = 'dashboard'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
